c054b38f404a208b26d403903fabfb6b71ad008d
ATHARVA-TIRKHUNDE <85235935+ATHARVA-TIRKHUNDE@users.noreply.github.com>
2023-10-14 12:30:24 +0530

    Update script.js env variable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const API_KEY = "848d4223fa69425f8b0c7c73ca186858" ;
+const API_KEY = process.env.API_URL ;
 const url = "https://newsapi.org/v2/everything?q=" ;
 
 window.addEventListener("load", () => fetchNews("India"));
